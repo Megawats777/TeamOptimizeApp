aa471c9590c664204b1711a9394283b5864a0672
Megawats777 <jedsemilla@gmail.com>
2016-04-10 13:53:42 -0400

    Added ability to switch between two cameras

--- Assets/Scripts/CameraSwitchManager.cs
+++ Assets/Scripts/CameraSwitchManager.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraSwitchManager : MonoBehaviour {
+public class CameraSwitchManager : MonoBehaviour
+{
+    /*--The Cameras in the level--*/
+    [SerializeField]
+    private GameObject topCamera;
+
+    [SerializeField]
+    private GameObject perspectiveCamera;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        // Disable the perspective camera
+        perspectiveCamera.SetActive(false);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    // Use the top camera
+    public void useTopCamera()
+    {
+        // Enable the top camera
+        topCamera.SetActive(true);
+
+        // Disable the perspective camera
+        perspectiveCamera.SetActive(false);
+    }
+
+    // Use the perspective camera
+    public void usePerspectiveCamera()
+    {
+        // Enable the perspective camera
+        perspectiveCamera.SetActive(true);
+
+        // Disable the top camera
+        topCamera.SetActive(true);
+    }
+
 }
